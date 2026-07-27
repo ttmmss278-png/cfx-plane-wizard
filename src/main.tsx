dca83b4d770f00e14bb41cb0eac7aaa5bc4f0bdf
ttmmss278-png <ttmmss278@gmail.com>
2026-07-27 20:37:54 +0800

    Load compact professional homepage workbench styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./styles.css";
 import "./ux-v2.css";
 import "./ux-v3.css";
 import "./topbar-polish.css";
+import "./home-workbench-polish.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
