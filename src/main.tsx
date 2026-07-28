452e6a3670a723c0e87310d8ff2e8b27a9321c78
ttmmss278-png <ttmmss278@gmail.com>
2026-07-28 10:03:06 +0800

    接入子模块视图稳定器

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import "./ux-v2.css";
 import "./ux-v3.css";
 import "./topbar-polish.css";
 import "./home-workbench-polish.css";
+import "./frame-stabilizer.css";
+import "./frame-stabilizer";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
